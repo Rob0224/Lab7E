750145824c83a36b2312c540988278a1915b60b1
Arthur Robinson <robinson0224@gmail.com>
2018-09-22 18:28:23 -0400

    Add project files.

---
+++ Vehicles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Console.WriteLine("Journey by airplane");
+            Airplane myPlane = new Airplane();
+            myPlane.StartEngine("Contact");
+            myPlane.TakeOff();
+            myPlane.Drive();
+            myPlane.Land();
+            myPlane.StopEngine("Phut phut");
+            Console.WriteLine("\nJourney by car:");
+            Car mycar = new Car();
+            mycar.StartEngine("Brm brm");
+            mycar.Accelerate();
+            mycar.Drive();
+            mycar.Brake();
+            mycar.StopEngine("Phut phut");
+            Console.WriteLine("\nTesting polymorphism");
+            Vehicle v = mycar;
+            v.Drive();
+            v = myPlane;
+            v.Drive();
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
---
+++ Vehicles/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles
+{
+    class Vehicle
+    {
+        public void StartEngine (string nosieTomakeWhenStarting)
+        {
+            Console.WriteLine($"Starting engine: {nosieTomakeWhenStarting}");
+        }
+
+        public void StopEngine (string noiseToMakeWhenStopping)
+        {
+            Console.WriteLine($"Stopping engine: {noiseToMakeWhenStopping}");
+        }
+
+        public virtual void Drive()
+        {
+            Console.WriteLine("Defualt implementation of the Drive method");
+        }
+    }
+}
